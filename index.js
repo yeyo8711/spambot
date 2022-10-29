d1fe9d047f7d025567dc31600d49493f967798de
Yeyo8711 <jair.carmona87@gmail.com>
2022-10-29 15:48:41 -0500

    restart

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
-const server = http.createServer(process.env.PORT || 8080);
+
 const express = require("express");
 
 const app = express();
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
 app.set("port", PORT);
 app.get("/", (req, res) => {
